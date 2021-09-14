07c65eb4f208a6ad19ddec20ba0c8aab0350d4eb
pelikhan <pelikhan@users.noreply.github.com>
2021-09-14 19:58:03 +0000

    deploy: 23dd16765fdc23b0bf19272c1bd7e691a52cd8a2

--- app-022aefdd74762bf95ee4.js
+++ app-022aefdd74762bf95ee4.js
@@ -63469,7 +63469,7 @@ function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { va
 
 
 var repo = "microsoft/jacdac-docs";
-var sha = "25c3f0f2ac31b1956e70143b9f3c8ebafedcd511";
+var sha = "23dd16765fdc23b0bf19272c1bd7e691a52cd8a2";
 
 function splitProperties(props) {
   if (!props) return {};
@@ -64349,7 +64349,7 @@ var useStyles = (0,makeStyles/* default */.Z)(theme => (0,createStyles/* default
 function Footer() {
   var classes = useStyles();
   var repo = "microsoft/jacdac-docs";
-  var sha = "25c3f0f2ac31b1956e70143b9f3c8ebafedcd511";
+  var sha = "23dd16765fdc23b0bf19272c1bd7e691a52cd8a2";
   return /*#__PURE__*/react.createElement("footer", {
     role: "contentinfo",
     className: classes.footer
@@ -81724,4 +81724,4 @@ module.exports = JSON.parse('{"layout":"constrained","backgroundColor":"#f8f8f8"
 /******/ var __webpack_exports__ = __webpack_require__.O();
 /******/ }
 ]);
-//# sourceMappingURL=app-ec8137bb6d08835ae459.js.map
+//# sourceMappingURL=app-022aefdd74762bf95ee4.js.map
--- component---src-pages-clients-p-5-js-mdx-85bbe1d2262b9ebbd614.js
+++ component---src-pages-clients-p-5-js-mdx-85bbe1d2262b9ebbd614.js
@@ -67,14 +67,44 @@ function MDXContent(_ref) {
     "href": "https://p5js.org",
     "target": "_blank",
     "rel": "nofollow noopener noreferrer"
-  }, "p5.js"), " to create creative coding with physical devices."), (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("ul", null, (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("li", {
+  }, "p5.js"), " to create creative coding with physical devices\nwith installing any drivers!"), (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("ul", null, (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("li", {
     parentName: "ul"
-  }, "in the online p5.js editor, ", (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("a", {
+  }, "no driver or installation needed"), (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("li", {
+    parentName: "ul"
+  }, "just works from your browser")), (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("h2", {
+    "id": "support-for-webusb-webserial",
+    "style": {
+      "position": "relative"
+    }
+  }, (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("a", {
+    parentName: "h2",
+    "href": "#support-for-webusb-webserial",
+    "aria-label": "support for webusb webserial permalink",
+    "className": "anchor before"
+  }, (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("svg", {
+    parentName: "a",
+    "aria-hidden": "true",
+    "focusable": "false",
+    "height": "16",
+    "version": "1.1",
+    "viewBox": "0 0 16 16",
+    "width": "16"
+  }, (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("path", {
+    parentName: "svg",
+    "fillRule": "evenodd",
+    "d": "M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"
+  }))), "Support for WebUSB, WebSerial"), (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("ul", null, (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("li", {
+    parentName: "ul"
+  }, "in the ", (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("a", {
     parentName: "li",
     "href": "https://editor.p5js.org/",
     "target": "_blank",
     "rel": "nofollow noopener noreferrer"
-  }, "https://editor.p5js.org/"), ", only WebSerial is supported at the moment. To use\nWebUSB or WebBluetooth, run p5js locally.")), (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("h2", {
+  }, "online p5.js editor"), ", only WebSerial is supported"), (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("li", {
+    parentName: "ul"
+  }, "in CodeSandbox, only WebUSB is supported"), (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("li", {
+    parentName: "ul"
+  }, "running p5.js locally? everything is possible!")), (0,_mdx_js_react__WEBPACK_IMPORTED_MODULE_1__.mdx)("h2", {
     "id": "usage",
     "style": {
       "position": "relative"
@@ -254,4 +284,4 @@ var Page = _ref => {
 /***/ })
 
 }]);
-//# sourceMappingURL=component---src-pages-clients-p-5-js-mdx-3250b7822c5157ac3f9b.js.map
+//# sourceMappingURL=component---src-pages-clients-p-5-js-mdx-85bbe1d2262b9ebbd614.js.map
