ae78b7b7e7d36359cd198f920a8e2716ff9ba590
hartley9 <k.hartley1@lancaster.ac.uk>
2022-05-08 21:53:00 +0100

    component representing a carrier pcb

---
+++ src/components/pf_editor/CarrierPCB.tsx
@@ -0,0 +1,22 @@
+import React, { useRef, useState } from "react";
+
+// Component representing instance of a carrier pcb, upon which module can be mounted
+export default function CarrierPCB(props: {height: number, width: number, rotation: number[], position: number[], color: string }) {
+
+    const carrierPCB = useRef();
+
+    const {height, color, width, position, rotation} = props
+    
+    return (
+        <>
+        <mesh ref={carrierPCB} position={position}  rotation={rotation} >
+            <boxBufferGeometry args={[width, height, 2]}/>
+            <meshStandardMaterial color={color} emissiveIntensity={0}/>
+        </mesh> 
+        </>
+    )
+
+
+
+
+}
