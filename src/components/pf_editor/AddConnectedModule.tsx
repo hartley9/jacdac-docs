3708871d12d4353b1e68b3d19e0ac2094b6cd98e
hartley9 <k.hartley1@lancaster.ac.uk>
2022-06-09 21:20:45 +0100

    fix bug with spawning multiple objects

--- src/components/pf_editor/AddConnectedModule.tsx
+++ src/components/pf_editor/AddConnectedModule.tsx
@@ -1,46 +1,80 @@
-import { Grid } from "@mui/material";
-import React from "react";
-import Button from "../ui/Button";
-import { deviceToModelMap } from "./deviceToModelMap";
+import { Grid } from "@mui/material"
+import React from "react"
+import Button from "../ui/Button"
+import { deviceToModelMap } from "./deviceToModelMap"
 
-import AddIcon from "@mui/icons-material/Add";
+import AddIcon from "@mui/icons-material/Add"
+import PF_Module from "../models/PF_Module"
+import { randomUInt } from "../../../jacdac-ts/src/jdom/random"
 
-export default function AddModule(props: {addModule, devices}){
-    const { addModule, devices } = props;
-    
+export default function AddModule(props: {
+    addModule
+    devices
+    objects
+    setObjects
+    setIsDragging
+    lastClicked
+    floorPlane
+    setLastClicked
+}) {
+    const {
+        addModule,
+        devices,
+        objects,
+        setObjects,
+        setIsDragging,
+        lastClicked,
+        floorPlane,
+        setLastClicked,
+    } = props
+
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-   // const open = Boolean(anchorEl);
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
 
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
+        setAnchorEl(event.currentTarget)
 
-        devices.forEach(device => {
+        //example of plug and populate
+        const moduleNames = ["slider", "slider", "button", "button"]
+        const moduleComponents = []
 
-            if (deviceToModelMap[device.productIdentifier.toString()]){
-                const foundDevice = deviceToModelMap[device.productIdentifier.toString()];
-                addModule(foundDevice)
-            }
+        //devices.forEach(device => {
+        moduleNames.forEach(item => {
+            const topLevelUUID = crypto.randomUUID()
+            moduleComponents.push(
+                <PF_Module
+                    name={topLevelUUID}
+                    moduleName={item}
+                    key={Math.random()}
+                    id={crypto.randomUUID()}
+                    setIsDragging={setIsDragging}
+                    setLastClicked={setLastClicked}
+                    lastClicked={lastClicked}
+                    floorPlane={floorPlane}
+                    position={[randomUInt(50), 8, randomUInt(50)]}
+                />
+            )
         })
-      };
+
+        setObjects([...objects, ...moduleComponents])
+    }
 
     return (
         <>
             <Grid item>
-            <Button
-                        id="add-connected-module-button"
-                        //aria-controls={open ? 'module-list-menu' : undefined}
-                        aria-haspopup="true"
-                      //  aria-expanded={open ? 'true' : undefined}
-                        variant="contained"
-                        disableElevation
-                        onClick={handleClick}
-                        endIcon={<AddIcon />}
-                    >
-                        Add Connected
-                    </Button>
+                <Button
+                    id="add-connected-module-button"
+                    //aria-controls={open ? 'module-list-menu' : undefined}
+                    aria-haspopup="true"
+                    //  aria-expanded={open ? 'true' : undefined}
+                    variant="contained"
+                    disableElevation
+                    onClick={handleClick}
+                    endIcon={<AddIcon />}
+                >
+                    Add Connected
+                </Button>
             </Grid>
-        
         </>
     )
 }
