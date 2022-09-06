e72dec4dbbe1297ae3934228f783fef9d014d824
hartley9 <k.hartley1@lancaster.ac.uk>
2022-09-06 21:05:18 +0100

    add columns to either side of editor

--- src/pages/tools/PFEditor.tsx
+++ src/pages/tools/PFEditor.tsx
@@ -18,7 +18,8 @@ import useDevices from "../../components/hooks/useDevices"
 import useBus from "../../jacdac/useBus"
 import AddConnectedModule from "../../components/pf_editor/AddConnectedModule"
 import EnclosureOptions from "../../components/pf_editor/EnclosureOptions"
-import {route} from "../../components/pf_editor/editFunctions"
+import { route } from "../../components/pf_editor/editFunctions"
+import InfoPanel from "../../components/pf_editor/InfoPanel"
 
 export default function Page() {
     // connected jacdac devices
@@ -47,14 +48,14 @@ export default function Page() {
         depth: 35,
     })
 
-    const [traces, setTraces] = useState({power: [], data: []})
+    const [traces, setTraces] = useState({ power: [], data: [] })
 
     // lock/unlock orbit
-    const [lockOrbit, setLockOrbit] = useState(false);
+    const [lockOrbit, setLockOrbit] = useState(false)
 
     // enclosure visible
     const [enclosureVisible, setEnclosureVisible] = useState(false)
-    const [enclosureOptionsOpen, setEnclosureOptionsOpen] = useState(false);
+    const [enclosureOptionsOpen, setEnclosureOptionsOpen] = useState(false)
 
     //array to hold all jacdac module objects
     const [objects, setObjects] = useState([])
@@ -80,7 +81,7 @@ export default function Page() {
     const [isDragging, setIsDragging] = useState(false)
     const floorPlane = new THREE.Plane(new THREE.Vector3(0, 1, 0), 0)
 
-    const [cheatChange, setCheatChange] = useState(100);
+    const [cheatChange, setCheatChange] = useState(100)
 
     const addModule = async moduleName => {
         console.log(editorScene)
@@ -101,7 +102,6 @@ export default function Page() {
                 scene={editorScene}
                 traces={traces}
                 setTraces={setTraces}
-
             />,
         ])
     }
@@ -109,126 +109,148 @@ export default function Page() {
     return (
         <>
             <h1 top="50px">Productizer</h1>
+            <Grid container spacing={4}>
+                <Grid item xs={1}></Grid>
 
-            <Grid container spacing={4} columns={3} direction="row">
-                
-            <Grid container spacing={4} columns={3} direction="column">
+                <Grid item xs={8}>
+                    <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                        <Grid container spacing={4} direction="row">
+                            <Grid item>
+                                <AddModule addModule={addModule} />
+                            </Grid>
+                            {bus.connected && (
+                                <Grid item>
+                                    <AddConnectedModule
+                                        addModule={addModule}
+                                        devices={devices}
+                                        objects={objects}
+                                        setObjects={setObjects}
+                                        setIsDragging={setIsDragging}
+                                        lastClicked={lastClicked}
+                                        floorPlane={floorPlane}
+                                        setLastClicked={setLastClicked}
+                                        scene={editorScene}
+                                        setCarrierPCBDimensions={
+                                            setCarrierPCBDimensions
+                                        }
+                                        route={route}
+                                        setCheatChange={setCheatChange}
+                                    ></AddConnectedModule>
+                                </Grid>
+                            )}
 
-            </Grid>
-                <Grid item>
-                    <AddModule addModule={addModule} />
-                </Grid>
-                {bus.connected && (  
-                    <Grid item>
-        
-                            <AddConnectedModule
-                                addModule={addModule}
-                                devices={devices}
-                                objects={objects}
-                                setObjects={setObjects}
-                                setIsDragging={setIsDragging}
-                                lastClicked={lastClicked}
-                                floorPlane={floorPlane}
-                                setLastClicked={setLastClicked}
-                                scene={editorScene}
-                                setCarrierPCBDimensions={setCarrierPCBDimensions} 
-                                route={route}
-                                setCheatChange={setCheatChange}
-                            ></AddConnectedModule>
-                        
-                    </Grid>
-                )}
+                            <Grid item>
+                                <PFToolbar
+                                    traces={traces}
+                                    carrierPCBDimensions={carrierPCBDimensions}
+                                    lastClicked={
+                                        lastClicked ? lastClicked : undefined
+                                    }
+                                    objectRefs={objects}
+                                    enclosureDimensions={enclosureDimensions}
+                                    scene={editorScene}
+                                    setEnclosureOptionsOpen={
+                                        setEnclosureOptionsOpen
+                                    }
+                                ></PFToolbar>
+                            </Grid>
 
-             <Grid item>
-                    <PFToolbar
-                        traces={traces}
-                        carrierPCBDimensions={carrierPCBDimensions}
-                        lastClicked={lastClicked ? lastClicked : undefined}
-                        objectRefs={objects}
-                        enclosureDimensions={enclosureDimensions}
-                        scene={editorScene}
-                        setEnclosureOptionsOpen={setEnclosureOptionsOpen}
-                    ></PFToolbar>
-                </Grid> 
+                            <Grid item>
+                                <PFToolPanel
+                                    lastClicked={
+                                        lastClicked ? lastClicked : undefined
+                                    }
+                                    objectRefs={objects}
+                                    addModule={addModule}
+                                    carrierPCBDimensions={carrierPCBDimensions}
+                                    setCarrierPCBDimensions={
+                                        setCarrierPCBDimensions
+                                    }
+                                    enclosureDimensions={enclosureDimensions}
+                                    setEnclosureDimensions={
+                                        setEnclosureDimensions
+                                    }
+                                    enclosureVisible={enclosureVisible}
+                                    setEnclosureVisible={setEnclosureVisible}
+                                    setEnclosureOptionsOpen={
+                                        setEnclosureOptionsOpen
+                                    }
+                                    scene={editorScene}
+                                    traces={traces}
+                                    setTraces={setTraces}
+                                    lockOrbit={lockOrbit}
+                                    setLockOrbit={setLockOrbit}
+                                    cheatChange={cheatChange}
+                                ></PFToolPanel>
+                            </Grid>
+                        </Grid>
 
-                <Grid item>
-                    <PFToolPanel
-                        lastClicked={lastClicked ? lastClicked : undefined}
-                        objectRefs={objects}
-                        addModule={addModule}
-                        carrierPCBDimensions={carrierPCBDimensions}
-                        setCarrierPCBDimensions={setCarrierPCBDimensions}
-                        enclosureDimensions={enclosureDimensions}
-                        setEnclosureDimensions={setEnclosureDimensions}
-                        enclosureVisible={enclosureVisible}
-                        setEnclosureVisible={setEnclosureVisible}
-                        setEnclosureOptionsOpen={setEnclosureOptionsOpen}
-                        scene={editorScene}
-                        traces={traces}
-                        setTraces={setTraces}
-                        lockOrbit={lockOrbit}
-                        setLockOrbit={setLockOrbit}
-                        cheatChange={cheatChange}
-                    ></PFToolPanel>
-                </Grid>
-            </Grid>
-
-            <Grid container spacing={2} columns={12} direction="row" padding="20px">
-                <Grid
-                    item
-                    xs={16}
-                    direction="column"
-                    alignItems="stretch"
-                    style={{
-                        position: "relative",
-                        overflow: "hidden",
-                        height: "100vh",
-                    }}
-                >
-                    <Box
-                        textAlign="center"
-                        height={
-                            "100%"
-                        } /* item xs={14} direction="column" alignItems="stretch" style={{overflow: "visible", height: "75%" }} */
-                    >
-
-                        {enclosureOptionsOpen &&
-                        
-                            <EnclosureOptions
-                            enclosureDimensions={enclosureDimensions}
-                            enclosureOptionsOpen={enclosureOptionsOpen}
-                            setEnclosureOptionsOpen={setEnclosureOptionsOpen}
-                            scene={editorScene}
-                            ></EnclosureOptions>
-                        }
-                        <Canvas
-                            onCreated={({ scene }) => {
-                                setEditorScene(scene)
-                            }}
-                            ref={canvasRef}
-                            style={{
-                                position: "relative",
-                                height: "75%",
-                                overflow: "visible",
-                            }}
-                            shadows
-                            onPointerMissed={function () {
-                                setLastClicked(null)
-                            }}
+                        <Grid
+                            container
+                            spacing={2}
+                            columns={12}
+                            direction="row"
+                            padding="20px"
                         >
-                            <ambientLight intensity={1} />
-                            <directionalLight
-                                intensity={1}
-                                castShadow
-                                shadow-mapSize-height={1512}
-                                shadow-mapSize-width={1512}
-                            />
-                            <color
-                                attach="background"
-                                args={["lightblue"]}
-                            ></color>
+                            <Grid
+                                item
+                                xs={16}
+                                direction="column"
+                                alignItems="stretch"
+                                style={{
+                                    position: "relative",
+                                    overflow: "hidden",
+                                    height: "100vh",
+                                }}
+                            >
+                                <Box
+                                    textAlign="center"
+                                    height={
+                                        "100%"
+                                    } /* item xs={14} direction="column" alignItems="stretch" style={{overflow: "visible", height: "75%" }} */
+                                >
+                                    {enclosureOptionsOpen && (
+                                        <EnclosureOptions
+                                            enclosureDimensions={
+                                                enclosureDimensions
+                                            }
+                                            enclosureOptionsOpen={
+                                                enclosureOptionsOpen
+                                            }
+                                            setEnclosureOptionsOpen={
+                                                setEnclosureOptionsOpen
+                                            }
+                                            scene={editorScene}
+                                        ></EnclosureOptions>
+                                    )}
+                                    <Canvas
+                                        onCreated={({ scene }) => {
+                                            setEditorScene(scene)
+                                        }}
+                                        ref={canvasRef}
+                                        style={{
+                                            position: "relative",
+                                            height: "75%",
+                                            overflow: "visible",
+                                        }}
+                                        shadows
+                                        onPointerMissed={function () {
+                                            setLastClicked(null)
+                                        }}
+                                    >
+                                        <ambientLight intensity={1} />
+                                        <directionalLight
+                                            intensity={1}
+                                            castShadow
+                                            shadow-mapSize-height={1512}
+                                            shadow-mapSize-width={1512}
+                                        />
+                                        <color
+                                            attach="background"
+                                            args={["lightblue"]}
+                                        ></color>
 
-                          {/*   <gridHelper
+                                        {/*   <gridHelper
                                 args={[
                                     100,
                                     10,
@@ -239,44 +261,54 @@ export default function Page() {
                                 rotation={[0, 0, 0]}
                             /> */}
 
-                            <CarrierPCB
-                                width={carrierPCBDimensions.width}
-                                height={carrierPCBDimensions.height}
-                                rotation={[(Math.PI / 2) * 3, 0, 0]}
-                                position={[0, 6, 0]}
-                                color={"green"}
-                            />
+                                        <CarrierPCB
+                                            width={carrierPCBDimensions.width}
+                                            height={carrierPCBDimensions.height}
+                                            rotation={[(Math.PI / 2) * 3, 0, 0]}
+                                            position={[0, 6, 0]}
+                                            color={"green"}
+                                        />
 
-                            {enclosureVisible && (
-                                <Enclosure
-                                    height={enclosureDimensions.height}
-                                    width={enclosureDimensions.width}
-                                    depth={enclosureDimensions.depth}
-                                />
-                            )}
+                                        {enclosureVisible && (
+                                            <Enclosure
+                                                height={
+                                                    enclosureDimensions.height
+                                                }
+                                                width={
+                                                    enclosureDimensions.width
+                                                }
+                                                depth={
+                                                    enclosureDimensions.depth
+                                                }
+                                            />
+                                        )}
 
-                            <Objects objects={objects}></Objects>
+                                        <Objects objects={objects}></Objects>
 
-                            <OrthographicCamera
-                                makeDefault
-                                zoom={5}
-                                position={[0, 40, 0]}
-                                near={-1000}
-                                far={10000}
-                                up={[1,0,0]}
-                                /* rotateZ={THREE.MathUtils.degToRad(-90)} */
-                            />
+                                        <OrthographicCamera
+                                            makeDefault
+                                            zoom={5}
+                                            position={[0, 40, 0]}
+                                            near={-1000}
+                                            far={10000}
+                                            up={[1, 0, 0]}
+                                            /* rotateZ={THREE.MathUtils.degToRad(-90)} */
+                                        />
 
-                            <OrbitControls
-                                
-                                minZoom={-10}
-                                maxZoom={50}
-                                enabled={!isDragging && !lockOrbit}
-                                
-                                
-                            /> 
-                        </Canvas>
+                                        <OrbitControls
+                                            minZoom={-10}
+                                            maxZoom={50}
+                                            enabled={!isDragging && !lockOrbit}
+                                        />
+                                    </Canvas>
+                                </Box>
+                            </Grid>
+                        </Grid>
                     </Box>
+                </Grid>
+
+                <Grid item xs={3}>
+                    <InfoPanel></InfoPanel>
                 </Grid>
             </Grid>
         </>
